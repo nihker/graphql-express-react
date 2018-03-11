403ef86e0bb36747c7c90b1253f1bab0d51fb16b
Nihad Keric <nihadkeric@gmail.com>
2018-03-11 17:47:03 +0100

    Add CompanyType and resolve function in UserType

--- schema/schema.js
+++ schema/schema.js
@@ -9,17 +9,30 @@ const {
 } = graphgl;
 
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString},
+        name: {type: GraphQLString},
+        description: { type: GraphQLString}
+    }
+});
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args){
+               return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                .then(res => res.data); 
+            }
+        }
     }
 });
-
-//
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
